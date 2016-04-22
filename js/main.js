545efab88c40392ef5a57daf1ca7c46988d74723
Marissa96 <marissa.boswall96@gmail.com>
2016-04-22 16:55:01 -0400

    Walleye, Worm Graph
    added more animation to Walleye to get it to stop and start when
    clicked
    
    used waypoint to animate the worm graph

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-var $walleye = $('.walleye');
 var $leaf = $('.leaf');
+var $canada = $('.big-canada');
+var $walleye = $('.walleye');
+var $wormGraph = $('.worm-graph')
 
 $leaf.on('click', function () {
   $('.leaf').toggleClass('slide');
+});
+
+$wormGraph.waypoint(function (direction) {
+  $wormGraph.addClass('js-svg-animate');
+}, { offset: 'bottom-in-view' });
+
+$walleye.on('click', function () {
+  $('.walleye').toggleClass('no-animate');
 });
